68e607c44662ac1c2c349f4a034760ab9855fc47
Shockk <shokku.ra@gmail.com>
2014-02-21 18:11:11 +0000

    Wrote value correctness tests

--- public/lab2/spec/rect.js
+++ public/lab2/spec/rect.js
@@ -5,4 +5,34 @@ describe('Rect', function()
 		r = new Rect(10, 20, 40, 30)
 		expect(r).toBeDefined()
 	})
+
+	describe('when created', function()
+	{
+		var r
+
+		beforeEach(function()
+		{
+			r = new Rect(10, 20, 40, 30)
+		})
+
+		it('has the correct value of `x`', function()
+		{
+			expect(r.x).toBe(10)
+		})
+
+		it('has the correct value of `y`', function()
+		{
+			expect(r.y).toBe(20)
+		})
+
+		it('has the correct value of `width`', function()
+		{
+			expect(r.width).toBe(40)
+		})
+
+		it('has the correct value of `height`', function()
+		{
+			expect(r.height).toBe(30)
+		})
+	})
 })
--- public/lab2/src/code.js
+++ public/lab2/src/code.js
@@ -1,7 +1,7 @@
 var Rect = function(x, y, w, h)
 {
-	this.x = x;
-	this.y = y;
-	this.width = w;
-	this.height = h;
+	this.x = x
+	this.y = y
+	this.width = w
+	this.height = h
 }
